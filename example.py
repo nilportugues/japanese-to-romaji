1742c5607dbe14be6f85d125e7ac0aac0c4bef40
root <contact@nilportugues.com>
2019-03-10 00:42:35 +0100

    working example

---
+++ example.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import MeCab
+from pykakasi import kakasi,wakati
+
+## Prepare the libs
+mecab_tagger = MeCab.Tagger("")
+
+kakasi = kakasi()
+kakasi.setMode("H","a") # Hiragana to ascii, default: no conversion
+kakasi.setMode("K","a") # Katakana to ascii, default: no conversion
+kakasi.setMode("J","a") # Japanese to ascii, default: no conversion
+
+## Prepare the input text
+text = u"ずっと前から言っていた"
+parsed = [[chunk.split('\t')[0], tuple(chunk.split('\t')[1].split(','))] for chunk in mecab_tagger.parse(text).splitlines()[:-1]]
+
+
+## Parse
+for i in parsed:
+    #now for each i[0] do romaji
+    conv = kakasi.getConverter()
+    result = conv.do(i[0])
+    print(i[0])
+    print(result)
+
---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install python3 python-dev python3-dev \
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python-pip
+
+sudo apt install python3-setuptools
+sudo apt install mecab
+sudo apt install mecab-python3
+sudo apt install mecab-naist-jdic
+sudo apt install libmecab-dev
+sudo apt install python3-pip
+
+
+
+pip3 install mecab-python3
+pip3 install pykakasi
