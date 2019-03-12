0cc742387efd7405d7cf994d49b8ae33d5c10e16
root <contact@nilportugues.com>
2019-03-12 21:07:55 +0100

    Perfect japanese!

--- api/services/japaneseToRomaji.py
+++ api/services/japaneseToRomaji.py
@@ -22,14 +22,23 @@ class JapaneseToRomaji():
         for line in lines:
             text = line
             chunklines = mecab_tagger.parse(text).splitlines()[:-1]
-            parsed = [[chunk.split('\t')[0], tuple(chunk.split('\t')[1].split(','))] for chunk in chunklines]
+
+            parsed = [[chunk.split('\t')[0], tuple(chunk.split('\t')[1].split(',')) ] for chunk in chunklines]
+
 
             ## Parse
             romanizedLine = []
             for i in parsed:
                 #now for each i[0] do romaji
                 conv = kakasi.getConverter()
-                result = conv.do(i[0])
+
+
+                ## try to get i[1][7], if not possible, get i[0]
+                if len(i) == 2 and len(i[1]) > 8:
+                    result = conv.do(i[1][7])
+                else:
+                    result = conv.do(i[0])
+
                 romanizedLine.append(result)
 
             pair = {}
